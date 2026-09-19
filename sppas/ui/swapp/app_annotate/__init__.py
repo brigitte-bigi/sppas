f4495004efb5a57243662781fdb2131748503006
Brigitte Bigi <contact@sppas.org>
2026-09-19 14:45:25 +0200

    sppas.ui.swapp.app_annotate: Added the package and its design dossier
    The classes of the model are those SPPAS already has: sppasParam,
    sppasAnnotationsManager, sppasLogFile and sppasAnnReport. What the
    application adds is the order the annotations are run in, read from
    what each one requires.
    
    The UML chapter holds one class diagram per component of the MVC. The
    view keeps the organisation and the names of the wx interface, with
    the book of its pages becoming sections of one page, and the actions
    standing beside the reports under a toggle group.
    
    sppas.ui.swapp.app_plugins, sppas.ui.swapp.services.options: Told what
    holds when SPPAS answers from a server.

---
+++ sppas/ui/swapp/app_annotate/__init__.py
@@ -0,0 +1,40 @@
+# -*- coding: UTF-8 -*-
+"""
+:filename: sppas.ui.swapp.app_annotate.__init__.py
+:author: Brigitte Bigi
+:contact: contact@sppas.org
+:summary: The init file of the annotate app package
+
+.. _This file is part of SPPAS: https://sppas.org/
+..
+    -------------------------------------------------------------------------
+
+     ######   ########   ########      ###      ######
+    ##    ##  ##     ##  ##     ##    ## ##    ##    ##     the automatic
+    ##        ##     ##  ##     ##   ##   ##   ##            annotation
+     ######   ########   ########   ##     ##   ######        and
+          ##  ##         ##         #########        ##        analysis
+    ##    ##  ##         ##         ##     ##  ##    ##         of speech
+     ######   ##         ##         ##     ##   ######
+
+    Copyright (C) 2011-2026  Brigitte Bigi, CNRS
+    Laboratoire Parole et Langage, Aix-en-Provence, France
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    This banner notice must not be removed.
+
+    -------------------------------------------------------------------------
+
+"""
